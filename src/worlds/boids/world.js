ae11401587561efdda3c4e52e37017c7171f2bc0
Mateo Van Damme <Mateo.VanDamme@Ugent.be>
2023-04-07 19:20:54 +0200

    add camera movement to boids

--- src/worlds/boids/world.js
+++ src/worlds/boids/world.js
@@ -4,6 +4,8 @@ import { createMeshCube } from './meshCube.js';
 import { createLights } from './lights.js';
 import { Boid } from './boid.js';
 import { BoidManager } from './boidManager.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 export default class World {
   constructor(main) {
@@ -26,6 +28,13 @@ export default class World {
     this.boidManager = new BoidManager(this.camera, this.scene, amount, boidSize, cubeSize, constantVel)
     this.loop.updatables.push(this.boidManager);
 
+    //Camera controls
+    const controls = new OrbitControls(this.camera, this.main.renderer.domElement);
+    controls.minDistance = 10;
+    controls.maxDistance = 100;
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.target = new THREE.Vector3(0, 0, 0);
+    controls.update();
   }
 }
 
@@ -33,7 +42,7 @@ function createCamera() {
   const camera = new THREE.PerspectiveCamera(
     35, // fov = Field Of View
     1, // dummy value for aspect ratio
-    0.1, // near clipping plane
+    0.1, // near clipping plane 
     100, // far clipping plane
   );
   camera.position.set(0, 0, 10);
