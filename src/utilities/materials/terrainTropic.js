04012a77f5185a7ca72d60723fdde4a82dc0b98b
chaems <casper.haems@ugent.be>
2023-04-12 23:49:37 +0200

    Change: remove normals from terrainTropic

--- src/utilities/materials/terrainTropic.js
+++ src/utilities/materials/terrainTropic.js
@@ -1,47 +1,28 @@
 import * as THREE from 'three';
 
+const onLoad = (texture) => {
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(0.05, 0.05);
+};
+
 // Define the two textures to blend
-const stone = new THREE.TextureLoader().load('assets/images/rock_wall_02_diff_1k.jpg');
-stone.wrapS = THREE.RepeatWrapping;
-stone.wrapT = THREE.RepeatWrapping;
-stone.repeat.set(0.05, 0.05); // scale down by a factor of 20
-const stoneNormal = new THREE.TextureLoader().load('assets/images/rock_wall_02_nor_gl_1k.png');
-stoneNormal.wrapS = THREE.RepeatWrapping;
-stoneNormal.wrapT = THREE.RepeatWrapping;
-stoneNormal.repeat.set(0.05, 0.05); // scale down by a factor of 20
+const stone = new THREE.TextureLoader().load('assets/images/rock_wall_02_diff_1k.jpg', onLoad);
 
 //*Grass
-const grass = new THREE.TextureLoader().load('assets/images/grass2-seamless2-bright.jpg');
-grass.wrapS = THREE.RepeatWrapping;
-grass.wrapT = THREE.RepeatWrapping;
-grass.repeat.set(0.05, 0.05); // scale down by a factor of 20
-const grassNormal = new THREE.TextureLoader().load('assets/images/coast_sand_rocks_02_nor_gl_1k.png');
-grassNormal.wrapS = THREE.RepeatWrapping;
-grassNormal.wrapT = THREE.RepeatWrapping;
-grassNormal.repeat.set(0.05, 0.05); // scale down by a factor of 20
+const grass = new THREE.TextureLoader().load('assets/images/grass2-seamless2-bright.jpg', onLoad);
 
 //*Dirt
-const dirt = new THREE.TextureLoader().load('assets/images/sand-seamless.jpg');
-dirt.wrapS = THREE.RepeatWrapping;
-dirt.wrapT = THREE.RepeatWrapping;
-dirt.repeat.set(0.05, 0.05); // scale down by a factor of 20
-const dirtNormal = new THREE.TextureLoader().load('assets/images/coast_sand_04_nor_gl_1k.png');
-dirtNormal.wrapS = THREE.RepeatWrapping;
-dirtNormal.wrapT = THREE.RepeatWrapping;
-dirtNormal.repeat.set(0.05, 0.05); // scale down by a factor of 20
+const dirt = new THREE.TextureLoader().load('assets/images/sand-seamless.jpg', onLoad);
 
 // Define the custom shader material
 export const terrainTropic = new THREE.ShaderMaterial({
     uniforms: {
         stoneTexture: { type: "t", value: stone },
-        stoneNormalMap: { type: "t", value: stoneNormal },
 
         grassTexture: { type: "t", value: grass },
-        grassNormalMap: { type: "t", value: grassNormal },
-        
+
         dirtTexture: { type: "t", value: dirt },
-        dirtNormalMap: { type: "t", value: dirtNormal },
-        
+
         waterLevel: { type: "float", value: -0.05 },
         rockLevel: { type: "float", value: 0.1 },
         sunColor: { type: "c", value: new THREE.Color(0xffffff) },
@@ -83,10 +64,6 @@ export const terrainTropic = new THREE.ShaderMaterial({
         uniform sampler2D grassTexture;
         uniform sampler2D dirtTexture;
         
-        uniform sampler2D stoneNormalMap;
-        uniform sampler2D grassNormalMap;
-        uniform sampler2D dirtNormalMap;
-        
         uniform float waterLevel;
         uniform float rockLevel;
         
@@ -105,23 +82,17 @@ export const terrainTropic = new THREE.ShaderMaterial({
             
             vec4 stoneTexel = texture2D(stoneTexture, vUv  * 20.0);
             vec4 grassTexel = texture2D(grassTexture, vUv * 20.0);
-            vec4 dirtTexel = texture2D(dirtTexture, vUv * 20.0);
-            
-            vec4 stoneNormal = texture2D(stoneNormalMap, vUv  * 20.0);
-            vec4 grassNormal = texture2D(grassNormalMap, vUv * 20.0);
-            vec4 dirtNormal = texture2D(dirtNormalMap, vUv * 20.0);            
+            vec4 dirtTexel = texture2D(dirtTexture, vUv * 20.0);       
             
             vec4 steepnessTexel = mix(stoneTexel, grassTexel, smoothstep(.5, 0.8, angle));
-            vec4 steepnessNormal = mix(stoneNormal, grassNormal, smoothstep(.5, 0.8, angle));
             
             vec4 texel = mix(dirtTexel,steepnessTexel, smoothstep(waterLevel, rockLevel, vPosition.z));
-            vec4 normalMap = mix(dirtNormal,steepnessNormal, smoothstep(waterLevel, rockLevel, vPosition.z));
             
             vec3 normal = normalize(vNormal);
             vec3 tangent = normalize(vTangent);
             vec3 bitangent = normalize(vBitangent);
             
-            vec3 mapNormal = normalize(vec3(normalMap.xyz * 2.0 - 1.0)) * mat3(bitangent, tangent, normal);
+            vec3 mapNormal = normalize(vec3(128.0)) * mat3(bitangent, tangent, normal);
             vec3 lightDirection = normalize(-sunDirection);
             float diffuse = max(0.0, dot(mapNormal, lightDirection)); 
             
