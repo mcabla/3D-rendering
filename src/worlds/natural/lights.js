9bfa216c4afb8e54f8ca59077001a6db44a822d0
chaems <casper.haems@ugent.be>
2023-05-07 01:40:47 +0200

    Fix: default lights position

--- src/worlds/natural/lights.js
+++ src/worlds/natural/lights.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 
 export function createLights() {
     const directionalLight = new THREE.DirectionalLight('white', 1);
-    directionalLight.position.set(10, 10, 10);
-    const ambientLight = new THREE.AmbientLight(0xfff9b2, .9);
+    directionalLight.position.set(0, 100, -100);
+    const ambientLight = new THREE.AmbientLight(0xfff9b2, .1);
     const group = new THREE.Group();
     group.add(directionalLight);
     group.add(ambientLight);
