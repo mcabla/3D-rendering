fbd51531cf4fca64c190ac8b964fb993686b4c5c
chaems <casper.haems@ugent.be>
2023-05-07 21:16:07 +0200

    Change: make the directional light a bit more blue

--- src/worlds/natural/lightDirectional.js
+++ src/worlds/natural/lightDirectional.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 export function createDirectionalLight() {
     // A point light works better in our case, but this isn't ideal for further distances.
-    const directionalLight = new THREE.PointLight(0xd2dcdf, 1.2, 0);
+    const directionalLight = new THREE.PointLight(0xd4d2df, 1.2, 0);
     directionalLight.position.set(0, 100, -100);
     return directionalLight;
 }
