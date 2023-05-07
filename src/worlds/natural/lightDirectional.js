21247b8674286db490f8313164116c1619d1760d
Mateo Van Damme <Mateo.VanDamme@Ugent.be>
2023-05-07 16:54:36 +0200

    'fix' terrain color

--- src/worlds/natural/lightDirectional.js
+++ src/worlds/natural/lightDirectional.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 export function createDirectionalLight() {
-    const directionalLight = new THREE.DirectionalLight('white', 1);
+    const directionalLight = new THREE.DirectionalLight('white', 2);
     directionalLight.position.set(0, 100, -100);
     return directionalLight;
 }
